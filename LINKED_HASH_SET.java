77bb2c3a2ff8188bdfacbae21ec55a4ebc0f7403
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-13 13:40:49 +0530

    Add files via upload

---
+++ LINKED_HASH_SET.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class LINKED_HASH_SET {
+    public static void main(String args[])
+    {
+        LinkedHashSet<String>a=new LinkedHashSet<>();
+        a.add("hello");
+        a.add("hello");
+        a.add("students");
+        a.add("sandhya");
+        System.out.println(a);
+        a.remove("hello");
+        System.out.println(a);
+
+    }
+}
+    
+
