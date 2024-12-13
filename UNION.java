0da137ae4619187ed9e2000ceceea2ae699a6d95
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-13 15:24:19 +0530

    Add files via upload

---
+++ UNION.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class UNION {
+    public static void main(String args[])
+    {
+        int arr1[]={22,33,44,55,66,1,2,3};
+        int arr2[]={22,33,4,1,2,23};
+        Set<Integer> a=new HashSet<>();
+        Set<Integer> b=new HashSet<>();
+        for(int s:arr1)
+        a.add(s);
+        for(int s1:arr2)
+        b.add(s1);
+        
+        System.out.println(a);
+        System.out.println(b);
+        Set<Integer> u=new HashSet<>();
+        u.addAll(b);
+        System.out.println(u);
+    }
+}
