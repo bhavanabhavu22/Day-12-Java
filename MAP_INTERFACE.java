467367f1e51759ba2c15f7ba1d24a758b5e52c5e
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-13 15:28:45 +0530

    Add files via upload

---
+++ MAP_INTERFACE.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class MAP_INTERFACE {
+    public static void main(String args[])
+    {
+        HashMap<String,String> a=new HashMap<>();
+        a.put("SNO","01");
+        a.put("sname","bavana");
+        System.out.println(a);
+    }
+    
+}
